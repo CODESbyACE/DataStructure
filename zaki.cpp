39fd8005cdd9e4c38107c30763f962a2fb02bc64
CODESbyACE <codesbyace@gmail.com>
2023-10-25 19:11:24 +0530

    zaki added

---
+++ zaki.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[10];
+    //Initialization
+    for(int i=0;i<4;i++){
+        cout << "Enter Value: ";
+        cin >> arr[i];
+    }
+
+    //Insertion
+    for(int i=4;i>=0;i--){
+    arr[i]=arr[i-1];
+  }
+  arr[0]=9;
+
+  //Deletion
+for ( int i = 2; i<4; i++)
+{
+    arr[i]=arr[i+1];
+    
+}
+//Traversing
+    for(int i=0;i<5;i++){
+        cout << "Value: ";
+        cout << arr[i]<<endl;
+    }
+
+    return 0;
+}
